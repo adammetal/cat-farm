f5fca9920a42017ee212e4ee41fb8f3f4e276264
adam.metal <adam.metal@codecool.com>
2023-01-31 11:48:11 +0100

    use the cat component in the app
    store the liked ones in a list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import './App.css'
+import { useState } from "react";
+import Cat from "./Cat";
+import "./App.css";
 
 function App() {
+  const [cats, setCats] = useState([]);
+
   return (
     <div className="App">
-      
+      <Cat
+        onYes={(src) => {
+          setCats((prev) => [...prev, src]);
+        }}
+      />
+      <div>
+        {cats.map((src) => (
+          <img key={src} src={src} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
